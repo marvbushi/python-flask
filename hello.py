fd3fc4fa495f1b3066f54da36e3a0b63e978cfeb
Marco Rodriguez <marvbushi@gmail.com>
2023-11-16 11:11:18 -0500

    form page

--- hello.py
+++ hello.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY']="super-secret-key"
 
-"""
-Jinja2:
-safe
-capitalize
-lower
-upper
-title
-trim
-striptags
-"""
+# Create a Form Class
+class NameForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route("/")
@@ -41,3 +39,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        
+    return render_template("name.html",
+                           name=name,
+                           form=form
+                           )
