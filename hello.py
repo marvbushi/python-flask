70acce7cd10b4c516ea35f9fbae5d7f2452033c6
Marco <m_rodriguez0@hotmail.com>
2023-11-17 11:36:33 -0500

    form message

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -50,8 +50,17 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash("Form Submitted Successfully")
         
     return render_template("name.html",
                            name=name,
                            form=form
                            )
+
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    # app.run()
+    app.run(debug=True)
